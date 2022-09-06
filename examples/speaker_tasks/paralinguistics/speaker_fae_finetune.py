60dfc5d4fd0c309d27241b8f1a4173d5feb8a537
Antonio Moreno <amorenodaniel@interactions.com>
2022-09-06 12:24:57 -0400

    applied formatter.

--- examples/speaker_tasks/paralinguistics/speaker_fae_finetune.py
+++ examples/speaker_tasks/paralinguistics/speaker_fae_finetune.py
@@ -13,22 +13,22 @@
 # limitations under the License.
 
 #
-# speaker_reco_finetune.py 
-#   --config-path=conf/ 
-#   --config-name=ecapa_tdnn_finetune-cv.yaml 
-#     model.train_ds.manifest_filepath=nemo_experiments/220823-Finetune-CV-validated/220825-Finetune-CV-eval/train-80-dlei-manifest.json 
-#     model.train_ds.augmentor.speed.prob=0.5 
-#     model.validation_ds.manifest_filepath=nemo_experiments/220823-Finetune-CV-validated/220825-Finetune-CV-eval/dev-80-dlei-manifest.json 
-#     model.test_ds.manifest_filepath=nemo_experiments/220823-Finetune-CV-validated/220825-Finetune-CV-eval/eval1-within-labels-dlei-manifest.json 
-#     trainer.max_epochs=5 
-#     trainer.devices='[1]' 
-#     model.decoder.num_classes=12 
+# speaker_reco_finetune.py
+#   --config-path=conf/
+#   --config-name=ecapa_tdnn_finetune-cv.yaml
+#     model.train_ds.manifest_filepath=nemo_experiments/220823-Finetune-CV-validated/220825-Finetune-CV-eval/train-80-dlei-manifest.json
+#     model.train_ds.augmentor.speed.prob=0.5
+#     model.validation_ds.manifest_filepath=nemo_experiments/220823-Finetune-CV-validated/220825-Finetune-CV-eval/dev-80-dlei-manifest.json
+#     model.test_ds.manifest_filepath=nemo_experiments/220823-Finetune-CV-validated/220825-Finetune-CV-eval/eval1-within-labels-dlei-manifest.json
+#     trainer.max_epochs=5
+#     trainer.devices='[1]'
+#     model.decoder.num_classes=12
 #     model.train_ds.batch_size=32
 #
 #  Manifests look like:
-#  { "audio_filepath": "", 
-#    "offset": 0, 
-#    "duration": 0, 
+#  { "audio_filepath": "",
+#    "offset": 0,
+#    "duration": 0,
 #    "label": "" }
 #
 #
@@ -48,18 +48,21 @@ seed_everything(42)
 @hydra_runner(config_path="conf", config_name="titanet-finetune.yaml")
 def main(cfg):
 
-    logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
+    logging.info(f"Hydra config: {OmegaConf.to_yaml(cfg)}")
     trainer = pl.Trainer(**cfg.trainer)
     _ = exp_manager(trainer, cfg.get("exp_manager", None))
     speaker_model = EncDecSpeakerLabelModel(cfg=cfg.model, trainer=trainer)
     speaker_model.maybe_init_from_pretrained_checkpoint(cfg)
     trainer.fit(speaker_model)
 
-    if hasattr(cfg.model, 'test_ds') and cfg.model.test_ds.manifest_filepath is not None:
+    if (
+        hasattr(cfg.model, "test_ds")
+        and cfg.model.test_ds.manifest_filepath is not None
+    ):
         trainer = pl.Trainer(devices=1, accelerator=cfg.trainer.accelerator)
         if speaker_model.prepare_test(trainer):
             trainer.test(speaker_model)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
